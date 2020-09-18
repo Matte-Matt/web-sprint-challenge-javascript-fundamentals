aa2c73ad37a5b317f468b5cb747f8b06c1f2c572
Ecclesia of Living Waters <eolw1.2901@gmail.com>
2020-09-18 14:57:22 -0400

    Final commit

--- challenges/closure.js
+++ challenges/closure.js
@@ -21,6 +21,12 @@ myFunction();
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
 
-function summation(parameter) {}
+function summation(parameter) {
+  let number = 0;
+  for (let i = 1; i <= parameter; i++) {
+    number = i + number;
+  }
+  return number;
+}
 
-console.log();
+console.log(summation(4));
